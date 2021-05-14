47551ed6ce5f7ca373cbc066a4a0f78491a555a7
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-05-14 09:38:32 +0200

    [ADD] - Order Page

--- js/app.js
+++ js/app.js
@@ -5,22 +5,25 @@ const header = document.querySelector('.header');
 const headerTop = document.querySelector('.header-top');
 const headerBottom = document.querySelector('.header-bottom');
 
-window.addEventListener('scroll', (e) => {
-  if (window.scrollY > 0 && (document.body.getBoundingClientRect()).top < scrollPos) {
-    header.classList.add('hide-top');
-    header.classList.add('show-bottom');
-  } else if (window.scrollY > 0 && (document.body.getBoundingClientRect()).top > scrollPos) {
-    header.classList.add('show-bottom');
-    header.classList.remove('hide-top');
-  }
+if (headerTop && headerBottom) {
+  window.addEventListener('scroll', (e) => {
+    if (window.scrollY > 0 && (document.body.getBoundingClientRect()).top < scrollPos) {
+      header.classList.add('hide-top');
+      header.classList.add('show-bottom');
+    } else if (window.scrollY > 0 && (document.body.getBoundingClientRect()).top > scrollPos) {
+      header.classList.add('show-bottom');
+      header.classList.remove('hide-top');
+    }
+  
+    if (window.scrollY == 0) {
+      header.classList.remove('hide-top');
+      header.classList.remove('show-bottom');
+    }
+  
+    scrollPos = (document.body.getBoundingClientRect()).top;
+  });
+}
 
-  if (window.scrollY == 0) {
-    header.classList.remove('hide-top');
-    header.classList.remove('show-bottom');
-  }
-
-  scrollPos = (document.body.getBoundingClientRect()).top;
-});
 
 /* #Option Loader
 ================================================== */
---
+++ js/order.js
@@ -0,0 +1,46 @@
+/* #Overview Dropdown
+================================================== */
+const overviewToggler = document.querySelector('.overview-toggler');
+const overviewDropdown = document.querySelector('.overview');
+
+overviewToggler.addEventListener('click', (e) => {
+  overviewDropdown.classList.toggle('show');
+  overviewToggler.classList.toggle('show');
+
+  if (overviewDropdown.classList.contains('show')) {
+    gsap.set(overviewDropdown, {overflow: 'auto'});
+    gsap.to(overviewDropdown, {height: '100%'});
+    gsap.set('body', {overflow: 'hidden'});
+  } else {
+    gsap.set('body', {overflow: 'visible'});
+    gsap.to(overviewDropdown, {height: '0'});
+    gsap.set(overviewDropdown, {overflow: 'hidden'});
+  }
+});
+
+/* #Form
+================================================== */
+const inputs = document.querySelectorAll('form input[type="text"]');
+const selects = document.querySelectorAll('form select');
+
+inputs.forEach((input) => {
+  input.addEventListener('input', (e) => {
+    if (input.value) {
+      input.closest('.field').classList.add('touched');
+    } else {
+      input.closest('.field').classList.remove('touched');
+    }
+  });
+});
+
+selects.forEach((select) => {
+  select.addEventListener('change', (e) => {
+    select.closest('.field').classList.add('touched');
+
+    if (select.value == '') {
+      select.closest('.field').classList.remove('touched');
+    } else {
+      select.closest('.field').classList.add('touched');
+    }
+  });
+});
